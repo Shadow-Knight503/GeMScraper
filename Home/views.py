30edae773b68435a3144dc27df59b59b7abcf854
CaptainAmerica73 <vijayasankar7397@gmail.com>
2024-02-22 22:50:34 +0530

    scrap more items

--- Home/views.py
+++ Home/views.py
@@ -23,18 +23,20 @@ def Scraper(url):
 def home(request):
     print("Initialising...")
     prod = []
-    res = Scraper('computers-0912tc')
-    soup = BeautifulSoup(res, 'html.parser')
-    for sup in soup.find_all('div', {'class': 'variant-wrapper'}):
-        ist = {
-            'ID': f"{sup.findChild('div', {'class': 'variant-image'}).findChild('a')['href'][-19:]}",
-            'Img': f"{sup.findChild('img')['src']}",
-            'Name': f"{sup.findChild('span', {'class': 'variant-title'}).text}",
-            'Price': sup.findChild('span', {'class': 'variant-final-price'}).text.replace('\n', ''),
-        }
-        ref = Db.collection("GeM").document(ist['ID'])
-        del ist['ID']
-        ref.set(ist)
-    print(prod)
+    for i in range(1, 20):
+        res = Scraper(f"#/?page={i}&_xhr=1")
+        soup = BeautifulSoup(res, 'html.parser')
+        for sup in soup.find_all('div', {'class': 'variant-wrapper'}):
+            ist = {
+                'ID': f"{sup.findChild('div', {'class': 'variant-image'}).findChild('a')['href'][-19:]}",
+                'Img': f"{sup.findChild('img')['src']}",
+                'Name': f"{sup.findChild('span', {'class': 'variant-title'}).text}",
+                'Price': sup.findChild('span', {'class': 'variant-final-price'}).text.replace('\n', ''),
+            }
+            ref = Db.collection("GeM").document(ist['ID'])
+            del ist['ID']
+            ref.set(ist)
+            prod.append(ist)
+        print(prod)
 
     return render(request, "Home.html", {'Prods': prod})
